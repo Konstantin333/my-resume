4600d1023d77b7d4978ad17c977e5a1878810fde
Konstantin333 <kp.dan95@gmail.com>
2026-04-08 15:53:13 +0400

    added new courses v.2

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,17 @@ const markers = document.querySelector(".markers");
 
 const allCourses = [
   "./images/courses/basics-of-programming.jpg",
+  "./images/courses/getting-to-know-the-platform.jpg",
   "./images/courses/start-in-1c.jpg"
 ];
-const allMarkers = [1, 3];
+const allMarkers = [1, 3, 5];
 
 course.parentNode.addEventListener("click", () =>
   window.open(course.getAttribute("src"))
 );
 
 function switchCourse() {
-  const arrInd = [0, 1];
+  const arrInd = [0, 1, 2];
 
   for (let i = 0; i < allMarkers.length; i++) {
     markers.childNodes[allMarkers[i]].onclick = function () {
